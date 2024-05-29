308341abc5d5726b2a44710e9074bd727f439059
shakeel.syed <shakeel.syed@gmail.com>
2024-05-29 16:17:39 -0700

    Added Welcome

--- LearnConsoleTracker/Program.cs
+++ LearnConsoleTracker/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (var i=0; i< 100; i++)
+{
     Console.WriteLine($"Code Tracker - {i}??");
+    Console.WriteLine($"Welcome - {i}");
+}
